caf2d51a53a787ea16d5f58557981841c9835861
mavs <wilhelm@localhost.localdomain>
2016-06-22 23:22:09 -0300

    starting

--- src/BakaTree.pm
+++ src/BakaTree.pm
@@ -6,13 +6,28 @@ package BakaTree;
 use 5.020;
 use warnings;
 
+use File::Basename;
+use File::chdir;
+
+use constant DIR  => 0;
+use constant FILE => 1;
+
 use parent qw(Exporter);
 
-our @EXPORT    = qw(greet);
+our @EXPORT    = qw(baka_tree);
 our @EXPORT_OK = ();
 
-sub greet {
-     say "Hi $_[0]";
+# recives a path and determines its tree structure
+sub baka_tree {
+     my ($basename, $path) = fileparse($_[0]);
+
+     local $CWD = "$path"; # go to path
+
+     my @bt;
+     $bt[0] = [{ name => $basename, type => DIR }];
+
+     say "$bt[0][0]{name} $bt[0][0]{type}";
+     say `ls`;
 }
 
 1; # end
